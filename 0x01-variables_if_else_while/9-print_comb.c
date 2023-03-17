7269f475ee275fbbcc782e14ed8056f842aac732
benson-kim <bensonkimkam@gmail.com>
2023-03-17 12:23:40 -0700

    update - adding space after ,

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,20 +7,17 @@
  */
 int main(void)
 {
-	int i, j;
+	int i;
 
-	for (i = 0; i < 9; i++)
+	for (i = 48; i < 58; i++)
 	{
-		for (j = i + 1; j <= 9; j++)
+		putchar(i);
+		if (i!=57)
 		{
-			putchar(i + '0');
 			putchar(',');
-			putchar(j + '0');
-			if (i != 8 || j != 9)
-			{
-				putchar(',');
-			}
+			putchar(' ');
 		}
 	}
+	putchar('\n');
 	return (0);
 }

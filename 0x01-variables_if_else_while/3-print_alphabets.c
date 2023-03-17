01101a9611546efb30d325a346d54eb784ab68d9
benson-kim <bensonkimkam@gmail.com>
2023-03-17 10:35:39 -0700

    update trailing whitespaces

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,21 +8,17 @@
 int main(void)
 {
 	char c;
-	
 	/* Print lowercase alphabet */
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		putchar(c);
 	}
-	
 	/* Print uppercase alphabet */
 	for (c = 'A'; c <= 'Z'; c++)
 	{
 		putchar(c);
 	}
-	
 	/* Print new line */
-	putchar('\n');
-	
+	putchar('\n');	
 	return (0);
 }

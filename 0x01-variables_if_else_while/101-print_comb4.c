24301622cc86a6008b96bcfb5c7fe05a4b0abb7e
benson-kim <bensonkimkam@gmail.com>
2023-03-17 12:40:27 -0700

    remove trailing whitespaces

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,27 +7,25 @@
  */
 int main(void)
 {
-    int i, j, k;
+	int i, j, k;
 
-    for (i = 0; i < 8; i++)
-    {
-        for (j = i + 1; j < 9; j++)
-        {
-            for (k = j + 1; k < 10; k++)
-            {
-                putchar((i % 3) + '0');
-                putchar(((j % 3) + 3 * (j / 3)) + '0');
-                putchar(((k % 3) + 6 * (k / 3)) + '0');
-
-                if (i != 7 || j != 8 || k != 9)
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-    }
-    putchar('\n');
-
-    return (0);
+	for (i = 0; i < 8; i++)
+	{
+		for (j = i + 1; j < 9; j++)
+		{
+			for (k = j + 1; k < 10; k++)
+			{
+				putchar((i % 3) + '0');
+				putchar(((j % 3) + 3 * (j / 3)) + '0');
+				putchar(((k % 3) + 6 * (k / 3)) + '0');
+				if (i != 7 || j != 8 || k != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }

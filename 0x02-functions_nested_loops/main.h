5bbb6ce2229c92d3309aab8738b1400fd9d1973e
benson-kim <bensonkimkam@gmail.com>
2023-03-21 03:13:43 -0700

    correct function prototype in header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,24 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include "main.h"
 
 int _islower(int c);
 int _putchar(char c);
-
-/**
- * print_alphabet_x10 - prints the alphabet 10 times in lowercase
- */
-void print_alphabet_x10(void)
-{
-	char letter;
-	int i;
-
-	for (i = 0; i < 10; i++)
-	{
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
-		_putchar('\n');
-	}
-}
+void print_alphabet_x10(void);
 
 #endif /* MAIN_H */

90ee819c2590ad0d0e2265fd3c6d9d3fa35aa645
benson-kim <bensonkimkam@gmail.com>
2023-03-21 13:05:15 +0300

    edit main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,25 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
 #include "main.h"
+
+int _islower(int c);
+int _putchar(char c);
 
 /**
  * print_alphabet_x10 - prints the alphabet 10 times in lowercase
  */
 void print_alphabet_x10(void)
 {
-	char letter;
-	int i;
+    char letter;
+    int i;
 
-	for (i = 0; i < 10; i++)
-	{
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
-		_putchar('\n');
-	}
+    for (i = 0; i < 10; i++)
+    {
+        for (letter = 'a'; letter <= 'z'; letter++)
+            _putchar(letter);
+        _putchar('\n');
+    }
 }
-
-#ifndef MAIN_H
-#define MAIN_H
-
-int _islower(int c);
-int _putchar(char c);
 
 #endif /* MAIN_H */

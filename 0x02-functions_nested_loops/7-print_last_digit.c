c4acab22a7e0b6253b64faa8524c3b0e7223f141
benson-kim <bensonkimkam@gmail.com>
2023-03-21 04:05:11 -0700

    fix errors and warnings

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include "main.h"
 
 /**
@@ -11,10 +10,11 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
-	/* get the last digit and make sure it's positive */
-	last_digit = _abs(n % 10);
+	if (n < 0 )
+		n = -n;
 
-	/* print the last digit */
+	last_digit = n % 10;
+
 	_putchar(last_digit + '0');
 
 	return (last_digit);

a765b18baa0f21a2597d6a387286c5e458235c31
benson-kim <bensonkimkam@gmail.com>
2023-03-21 10:40:36 -0700

    replace whitespaces

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,36 +8,36 @@
 
 int main(void)
 {
-    int i;
-    long int prev, curr, next;
+	int i;
+	long int prev, curr, next;
 
-    /* Initialize the first two Fibonacci numbers */
-    prev = 1;
-    curr = 2;
+	/* Initialize the first two Fibonacci numbers */
+	prev = 1;
+	curr = 2;
 
-    /* Print the first two Fibonacci numbers */
-    printf("%ld, %ld, ", prev, curr);
+	/* Print the first two Fibonacci numbers */
+	printf("%ld, %ld, ", prev, curr);
 
-    /* Compute and print the next 48 Fibonacci numbers */
-    for (i = 3; i <= 50; i++)
-    {
-        /* Compute the next Fibonacci number */
-        next = prev + curr;
+	/* Compute and print the next 48 Fibonacci numbers */
+	for (i = 3; i <= 50; i++)
+	{
+	    /* Compute the next Fibonacci number */
+	    next = prev + curr;
 
-        /* Print the next Fibonacci number */
-        printf("%ld", next);
+	    /* Print the next Fibonacci number */
+	    printf("%ld", next);
 
-        /* If this is not the last number, print a comma and space */
-        if (i < 50)
-            printf(", ");
+	    /* If this is not the last number, print a comma and space */
+	    if (i < 50)
+	        printf(", ");
 
-        /* Update the previous two Fibonacci numbers */
-        prev = curr;
-        curr = next;
-    }
+	    /* Update the previous two Fibonacci numbers */
+	    prev = curr;
+	    curr = next;
+	}
 
-    /* Print a newline to end the output */
-    printf("\n");
+	/* Print a newline to end the output */
+	printf("\n");
 
-    return 0;
+	return 0;
 }

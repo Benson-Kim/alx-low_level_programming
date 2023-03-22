49907c7ead2366d7283a220ec8bc40d232fe49f8
benson-kim <bensonkimkam@gmail.com>
2023-03-22 04:38:57 -0700

    fix whitespaces and idnetation

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,20 +8,19 @@
 
 int main(void)
 {
-    int i, sum;
+	int i, sum;
 
-    sum = 0;
+	sum = 0;
 
-    /* Iterate over all numbers below 1024 */
-    for (i = 0; i < 1024; i++)
-    {
-        /* If i is a multiple of 3 or 5, add it to the sum */
-        if (i % 3 == 0 || i % 5 == 0)
-            sum += i;
-    }
+	/* Iterate over all numbers below 1024 */
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += i;
+	}
 
-    /* Print the sum */
-    printf("%d\n", sum);
+	/* Print the sum */
+	printf("%d\n", sum);
 
-    return 0;
+	return (0);
 }

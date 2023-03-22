051151a5a97a73baccd4d8ee8d22bc912667997e
benson-kim <bensonkimkam@gmail.com>
2023-03-22 04:51:48 -0700

    a program that prints the first 98 fib numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,35 +2,26 @@
 
 /**
  * main - Finds and prints the sum of even-valued terms in the Fibonacci sequence
- *        up to 4,000,000
+ *	    up to 4,000,000
  *
  * Return: 0 on success
  */
 
 int main(void)
 {
-    int i;
-    unsigned int fib1 = 1, fib2 = 2, next, sum = 0;
+	unsigned long int a = 1, b = 2, c;
 
-    /* Add the first two Fibonacci numbers to the sum if they are even */
-    if (fib2 % 2 == 0)
-        sum += fib2;
+	printf("%lu, %lu", a, b);
 
-    /* Compute the remaining Fibonacci numbers and add the even ones to the sum */
-    for (i = 3; fib2 <= 4000000; i++)
-    {
-        next = fib1 + fib2;
+	for (int i = 2; i < 98; i++)
+	{
+		c = a + b;
+		printf(", %lu", c);
+		a = b;
+		b = c;
+	}
 
-        /* Add the next Fibonacci number to the sum if it is even */
-        if (next % 2 == 0)
-            sum += next;
+	printf("\n");
 
-        /* Shift the variables to prepare for the next iteration */
-        fib1 = fib2;
-        fib2 = next;
-    }
-
-    printf("%u\n", sum);
-
-    return 0;
+	return (0);
 }

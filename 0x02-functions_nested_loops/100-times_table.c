dd0af67a7191b42d3e41866a53f0df571e60171b
benson-kim <bensonkimkam@gmail.com>
2023-03-22 02:46:10 -0700

    remove trailing whitespaces

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,19 +8,19 @@
  */
 void print_times_table(int n)
 {
-    int i, j;
+	int i, j;
 
-    if (n < 0 || n > 15) /* Check if n is within the valid range */
-        return;
+	if (n < 0 || n > 15) /* Check if n is within the valid range */
+	    return;
 
-    for (i = 0; i <= n; i++)
-    {
-        for (j = 0; j <= n; j++)
-        {
-            printf("%d", i * j); /* Print the product of i and j */
-            if (j < n)           /* Print a tab character if not at the last column */
-                printf("\t");
-        }
-        printf("\n"); /* Move to the next row */
-    }
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			printf("%d", i * j); /* Print the product of i and j */
+			if (j < n)           /* Print a tab character if not at the last column */
+				printf(",\t");
+		}
+		printf("\n"); /* Move to the next row */
+	}
 }

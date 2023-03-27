16b43c824b6d39de805e33c56acf60d993d54b65
benson-kim <bensonkimkam@gmail.com>
2023-03-26 23:07:53 -0700

    break column length

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * _strcpy - Copies the string pointed to by src, including the terminating null byte (\0),
- *		   to the buffer pointed to by dest.
+ * _strcpy - Copies the string pointed to by src,
+ *	including the terminating null byte (\0),
+ *	to the buffer pointed to by dest.
  * @dest: Pointer to the destination buffer
  * @src: Pointer to the source string
  *

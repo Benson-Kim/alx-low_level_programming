b9aa2f6f67f1ec99d6e9737175029809e953e90a
benson-kim <bensonkimkam@gmail.com>
2023-03-26 23:40:18 -0700

    add check for long strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,10 +10,13 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i += 2)
 	{
 		putchar(str[i]);
-		i += 2;
+
+
+		if (str[i+1] == '\0')
+			break;
 	}
 
 	putchar('\n');

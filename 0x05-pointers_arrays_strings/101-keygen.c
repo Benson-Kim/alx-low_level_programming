0b98b0b92ed6d1b9575591ef9b1e2d96ccdeaa31
benson-kim <bensonkimkam@gmail.com>
2023-03-28 12:37:56 -0700

    differnt solution

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -6,55 +6,62 @@
 
 int main(void)
 {
-	const char valid_characters[] = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	const int num_valid_characters = sizeof(valid_characters) - 1;
-	char password[PASSWORD_LENGTH + 1];
-	int i;
-	int has_lower = 0;
-	int has_upper = 0;
-	int has_digit = 0;
+    char password[PASSWORD_LENGTH + 1];
+    int i;
+    int has_lower = 0;
+    int has_upper = 0;
+    int has_digit = 0;
 
-	srand(time(NULL));
+    srand(time(NULL));
 
-	for (i = 0; i < PASSWORD_LENGTH; i++) {
-		password[i] = valid_characters[rand() % num_valid_characters];
-		if ('a' <= password[i] && password[i] <= 'z') {
-			has_lower = 1;
-		}
-		else if ('A' <= password[i] && password[i] <= 'Z') {
-			has_upper = 1;
-		}
-		else if ('0' <= password[i] && password[i] <= '9') {
-			has_digit = 1;
-		}
-	}
-	password[PASSWORD_LENGTH] = '\0';
+    // Generate password
+    for (i = 0; i < PASSWORD_LENGTH; i++) {
+        int r = rand() % 62;
+        if (r < 10) {
+            password[i] = '0' + r;
+            has_digit = 1;
+        }
+        else if (r < 36) {
+            password[i] = 'A' + (r - 10);
+            has_upper = 1;
+        }
+        else {
+            password[i] = 'a' + (r - 36);
+            has_lower = 1;
+        }
+    }
+    password[PASSWORD_LENGTH] = '\0';
 
-	if (has_lower && has_upper && has_digit) {
-		printf("%s\n", password);
-	}
-	else {
-		while (!has_lower || !has_upper || !has_digit) {
-			has_lower = 0;
-			has_upper = 0;
-			has_digit = 0;
-			for (i = 0; i < PASSWORD_LENGTH; i++) {
-				password[i] = valid_characters[rand() % num_valid_characters];
-				if ('a' <= password[i] && password[i] <= 'z') {
-					has_lower = 1;
-				}
-				else if ('A' <= password[i] && password[i] <= 'Z') {
-					has_upper = 1;
-				}
-				else if ('0' <= password[i] && password[i] <= '9') {
-					has_digit = 1;
-				}
-			}
-			password[PASSWORD_LENGTH] = '\0';
-		}
-		printf("%s\n", password);
-	}
+    // Check if password meets requirements
+    if (has_lower && has_upper && has_digit) {
+        printf("%s\n", password);
+    }
+    else {
+        // Regenerate password until it meets requirements
+        while (!has_lower || !has_upper || !has_digit) {
+            has_lower = 0;
+            has_upper = 0;
+            has_digit = 0;
+            for (i = 0; i < PASSWORD_LENGTH; i++) {
+                int r = rand() % 62;
+                if (r < 10) {
+                    password[i] = '0' + r;
+                    has_digit = 1;
+                }
+                else if (r < 36) {
+                    password[i] = 'A' + (r - 10);
+                    has_upper = 1;
+                }
+                else {
+                    password[i] = 'a' + (r - 36);
+                    has_lower = 1;
+                }
+            }
+            password[PASSWORD_LENGTH] = '\0';
+        }
+        printf("%s\n", password);
+    }
 
-	return 0;
+    return 0;
 }
 

8ed18b55a02ce1c74f0f590c815e7f050307a59b
benson-kim <bensonkimkam@gmail.com>
2023-03-30 00:55:38 -0700

    * _strncat - Concatenates two strings.  * @dest: Destination string.  * @src: Source string.  * @n: Number of bytes to be appended.  *  * Return: Pointer to the resulting string

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,22 +10,22 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-    int dest_len = 0, i = 0;
+	int dest_len = 0, i = 0;
 
-    /* Calculate the length of the destination string */
-    while (dest[dest_len] != '\0')
-        dest_len++;
+	/* Calculate the length of the destination string */
+	while (dest[dest_len] != '\0')
+		dest_len++;
 
-    /* Append the source string to the destination string */
-    while (i < n && src[i] != '\0')
-    {
-        dest[dest_len] = src[i];
-        i++;
-        dest_len++;
-    }
+	/* Append the source string to the destination string */
+	while (i < n && src[i] != '\0')
+	{
+		dest[dest_len] = src[i];
+		i++;
+		dest_len++;
+	}
 
-    /* Add the terminating null byte */
-    dest[dest_len] = '\0';
+	/* Add the terminating null byte */
+	dest[dest_len] = '\0';
 
-    return (dest);
+	return (dest);
 }

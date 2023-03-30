869f290d80c8246b5af402ed44f41afc4d2a7fc6
benson-kim <bensonkimkam@gmail.com>
2023-03-30 01:01:46 -0700

    a function that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,22 +8,22 @@
  */
 char *cap_string(char *str)
 {
-    char *p = str;
+	char *p = str;
 
-    /* Iterate over each character of the string */
-    while (*p)
-    {
-        /* If character is lowercase and its preceding character is a word separator, convert it to uppercase */
-        if (*p >= 'a' && *p <= 'z' &&
-            (*(p - 1) == ' ' || *(p - 1) == '\t' || *(p - 1) == '\n' ||
-             *(p - 1) == ',' || *(p - 1) == ';' || *(p - 1) == '.' ||
-             *(p - 1) == '!' || *(p - 1) == '?' || *(p - 1) == '"' ||
-             *(p - 1) == '(' || *(p - 1) == ')' || *(p - 1) == '{' ||
-             *(p - 1) == '}'))
-            *p -= 32;
+	/* Iterate over each character of the string */
+	while (*p)
+	{
+		/* If character is lowercase and its preceding character is a word separator, convert it to uppercase */
+		if (*p >= 'a' && *p <= 'z' &&
+			(*(p - 1) == ' ' || *(p - 1) == '\t' || *(p - 1) == '\n' ||
+			 *(p - 1) == ',' || *(p - 1) == ';' || *(p - 1) == '.' ||
+			 *(p - 1) == '!' || *(p - 1) == '?' || *(p - 1) == '"' ||
+			 *(p - 1) == '(' || *(p - 1) == ')' || *(p - 1) == '{' ||
+			 *(p - 1) == '}'))
+			*p -= 32;
 
-        p++; /* Move to the next character */
-    }
+		p++; /* Move to the next character */
+	}
 
-    return (str);
+	return (str);
 }

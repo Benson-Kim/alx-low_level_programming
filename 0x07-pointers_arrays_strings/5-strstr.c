ddf41250d20d2e16111a2a6ec8ab59bbf408998e
benson-kim <bensonkimkam@gmail.com>
2023-04-03 11:17:45 -0700

    function finds the first occurrence of the substring needle in the string haystack

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,25 +10,25 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-  char *h, *n;
+	char *h, *n;
 
-  while (*haystack != '\0')
-  {
-    h = haystack;
-    n = needle;
+	while (*haystack != '\0')
+	{
+		h = haystack;
+		n = needle;
 
-    while (*n != '\0' && *n == *h)
-    {
-      h++;
-      n++;
-    }
+		while (*n != '\0' && *n == *h)
+		{
+			h++;
+			n++;
+		}
 
-    if (*n == '\0')
-      return (haystack);
+		if (*n == '\0')
+			return (haystack);
 
-    haystack++;
-  }
+		haystack++;
+	}
 
-  return (NULL);
+	return (NULL);
 }
 

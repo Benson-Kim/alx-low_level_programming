4b4c5e51b53a40ca2670f94f259928550268a44d
benson-kim <bensonkimkam@gmail.com>
2023-04-14 00:35:33 -0700

    correct Betty's coding style warnings

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,23 +13,22 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *new_ptr, *old_ptr;
 	unsigned int i;
 
-	if (new_size == 0 && ptr != NULL) {
-	        free(ptr);
-	        return (NULL);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
 	}
+	
+	if (ptr == NULL)
+		return (malloc(new_size));
 
-	if (ptr == NULL) {
-	        return (malloc(new_size));
-	}
-
-	if (new_size == old_size) {
-	        return (ptr);
-	}
+	if (new_size == old_size)
+		return (ptr);
 
 	new_ptr = malloc(new_size);
-	if (new_ptr == NULL) {
-	        return (NULL);
-	}
+
+	if (new_ptr == NULL)
+		return (NULL);
 
 	old_ptr = ptr;
 	for (i = 0; i < old_size && i < new_size; i++) {

ee6f1da4fa80cd6030b921a475773b2dbd20e4f3
benson-kim <bensonkimkam@gmail.com>
2023-04-14 01:37:47 -0700

    a program that mul two numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -7,105 +7,106 @@
  *
  * Return: 1 if c is a digit, 0 otherwise
  */
-int _isdigit(char c)
+int _isdigit(int c)
 {
 	return (c >= '0' && c <= '9');
 }
 
 /**
- * _strlen - calculates the length of a string
- * @s: the string to calculate the length of
+ * _strlen - computes the length of a string
+ * @s: the string to measure
  *
- * Return: the length of the string
+ * Return: the length of s
  */
-int _strlen(char *s)
+size_t _strlen(const char *s)
 {
-	int len = 0;
+	size_t len = 0;
 
 	while (*s++)
-		len++;
+	    len++;
+
 	return (len);
 }
 
 /**
  * mul - multiplies two positive numbers
- * @num1: the first number to multiply
- * @num2: the second number to multiply
+ * @num1: the first number
+ * @num2: the second number
  *
- * Return: a pointer to the result, or NULL if an error occurs
+ * Return: a pointer to the result, or NULL on failure
  */
-char *mul(char *num1, char *num2)
+char *mul(const char *num1, const char *num2)
 {
-	int len1 = _strlen(num1);
-	int len2 = _strlen(num2);
-	int i, j, k, carry, sum;
-	char *result;
+	size_t len1, len2, len_res, i, j;
+	int *res, carry;
 
-	if (!num1 || !num2 || !*num1 || !*num2)
-		return (NULL);
+	if (!num1 || !num2 || !_isdigit(*num1) || !_isdigit(*num2))
+	    return (NULL);
 
-	result = malloc(len1 + len2 + 1);
-	if (!result)
-		return (NULL);
-	for (i = 0; i < len1 + len2; i++)
-		result[i] = '0';
-	result[i] = '\0';
+	len1 = _strlen(num1);
+	len2 = _strlen(num2);
+	len_res = len1 + len2;
+	res = calloc(len_res, sizeof(int));
+	if (!res)
+	    return (NULL);
 
-	for (i = len1 - 1; i >= 0; i--)
+	for (i = len1; i--;)
 	{
-		if (!_isdigit(num1[i]))
-		        return (NULL);
-		carry = 0;
-		for (j = len2 - 1, k = i + j + 1; j >= 0; j--, k--)
-		{
-		        if (!_isdigit(num2[j]))
-		                return (NULL);
-		        sum = (num1[i] - '0') * (num2[j] - '0') + (result[k] - '0') + carry;
-		        carry = sum / 10;
-		        result[k] = (sum % 10) + '0';
-		}
-		result[k] = carry + '0';
+	    if (!_isdigit(num1[i]))
+	        goto error;
+
+	    carry = 0;
+	    for (j = len2; j--;)
+	    {
+	        if (!_isdigit(num2[j]))
+	            goto error;
+
+	        res[i + j + 1] += (num1[i] - '0') * (num2[j] - '0') + carry;
+	        carry = res[i + j + 1] / 10;
+	        res[i + j + 1] %= 10;
+	    }
+	    res[i] += carry;
 	}
 
-	while (*result == '0' && *(result + 1))
-		result++;
+	while (*res == 0 && len_res > 1)
+	{
+	    res++;
+	    len_res--;
+	}
 
-	return (result);
+	return ((char *)res);
+
+error:
+	free(res);
+	return (NULL);
 }
 
 /**
- * main - multiplies two positive numbers passed as arguments
- * @argc: the number of arguments passed to the program
- * @argv: an array of strings containing the arguments
+ * main - multiplies two positive numbers
+ * @argc: the argument count
+ * @argv: the argument vector
  *
- * Return: 0 if successful, 98 if an error occurs
+ * Return: 0 if successful, 98 if there is an error
  */
 int main(int argc, char **argv)
 {
-	char *result;
+	char *res;
 
 	if (argc != 3)
 	{
-		printf("Error\n");
-		return (98);
+	    printf("Error\n");
+	    return (98);
 	}
 
-	if (!*argv[1] || !*argv[2])
+	res = mul(argv[1], argv[2]);
+	if (!res)
 	{
-		printf("Error\n");
-		return (98);
+	    printf("Error\n");
+	    return (98);
 	}
 
-	result = mul(argv[1], argv[2]);
+	printf("%s\n", res);
+	free(res);
 
-	if (!result)
-	{
-		printf("Error\n");
-		return (98);
-	}
-
-	printf("%s\n", result);
-	free(result);
 	return (0);
 }
-

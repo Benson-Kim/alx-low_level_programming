e4be3408f0955d2ad5eab1a0992b1f609f4c88c4
Benson <55843982+Benson-Kim@users.noreply.github.com>
2023-04-15 12:39:04 +0300

    Update main.h

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void print_program_name(char *argv[]);
+void print_arguments(int argc, char *argv[]);
+int count_arguments(int argc);
+int multiply(int num1, int num2);
+int add_positive_numbers(int argc, char *argv[]);
+
+#endif /* MAIN_H */

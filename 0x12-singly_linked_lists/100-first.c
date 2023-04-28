f5a51595499c8d6164e5192835480c2cad6518b1
benson-kim <bensonkimkam@gmail.com>
2023-04-28 09:30:08 -0700

    redefine betty's style

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,18 +1,25 @@
 /**
- * print_message - Prints a message before the main function is executed.
+ * print_message - Prints a message before main is executed
  *
  * Description: This function is executed automatically before the main
- * function is called, and prints the message
- * "You're beat! and yet, you must allow,"
- * followed by "I bore my house upon my back!" on separate lines.
+ * function is called. It prints the message "You're beat! and yet, you must
+ * allow," followed by "I bore my house upon my back!" on separate lines.
  *
  * Return: void
  */
-
 #include <stdio.h>
 
 void print_message(void) __attribute__ ((constructor));
 
+/**
+ * print_message - Prints a message before main is executed
+ *
+ * Description: This function is executed automatically before the main
+ * function is called. It prints the message "You're beat! and yet, you must
+ * allow," followed by "I bore my house upon my back!" on separate lines.
+ *
+ * Return: void
+ */
 void print_message(void)
 {
     printf("You're beat! and yet, you must allow,\n");

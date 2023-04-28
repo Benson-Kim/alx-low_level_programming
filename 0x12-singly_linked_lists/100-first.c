1bf173977669df08e743e75579861ea332941ff6
benson-kim <bensonkimkam@gmail.com>
2023-04-28 00:33:33 -0700

    redefining

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,8 +1,18 @@
+/**
+ * print_message - Prints a message before the main function is executed.
+ *
+ * Description: This function is executed automatically before the main
+ * function is called, and prints the message "You're beat! and yet, you
+ * must allow," followed by "I bore my house upon my back!" on separate lines.
+ *
+ * Return: void
+ */
 #include <stdio.h>
 
 void print_message(void) __attribute__ ((constructor));
 
-void print_message(void) {
+void print_message(void)
+{
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }

f3851c71b6359ff3f1c3992ddd95de31ecc750eb
benson-kim <bensonkimkam@gmail.com>
2023-04-28 09:24:51 -0700

    add return type

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,14 +7,15 @@
  *
  * Return: void
  */
+
 #include <stdio.h>
 
 void print_message(void) __attribute__ ((constructor));
 
-void print_message(void) {
 void print_message(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
-	return;
+    printf("You're beat! and yet, you must allow,\n");
+    printf("I bore my house upon my back!\n");
+    return;
 }
+
